809dea3559a3d73cba193373f672a5f447e5f158
desareca <43764566+desareca@users.noreply.github.com>
2025-04-03 20:33:27 -0300

    fix gif game of life

--- src/lib/data/projects.ts
+++ src/lib/data/projects.ts
@@ -148,6 +148,10 @@ const items: Array<Project> = [
 		description:
 			`El Juego de la vida es un autómata celular diseñado por el matemático británico John Horton Conway en 1970. Es un juego de cero jugadores, en el que su evolución es determinada por un estado inicial, sin requerir intervención adicional.
 			<br><br>
+			<div style="display: flex; justify-content: center; text-align: center;">
+			<img width="50%" height="auto" src='https://miro.medium.com/v2/resize:fit:720/format:webp/1*6wS-kXlOCBsAJciygkJtDA.gif'>
+			</div>
+			<br><br>	
 			El juego se desarrolla en una cuadrícula de celdas, donde cada celda puede estar viva o muerta. En cada paso del tiempo, el estado de cada celda se determina por el estado de sus ocho vecinos (celdas adyacentes). Las reglas son las siguientes:
 			<ul>
 			<li>Una celda viva con menos de dos vecinos vivos muere (subpoblación).
