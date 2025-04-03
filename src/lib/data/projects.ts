cb6a0dbf0977464fb158d0ae774e46ab5e7be386
desareca <43764566+desareca@users.noreply.github.com>
2025-04-03 20:04:34 -0300

    fix image analisis twitter

--- src/lib/data/projects.ts
+++ src/lib/data/projects.ts
@@ -386,9 +386,8 @@ const items: Array<Project> = [
 		color: 'royalblue',
 		description:
 			`<div style="display: flex; justify-content: center; text-align: center;">
-			<img width="70%" height="auto" src='https://github.com/desareca/Analisis-Sentimientos-Noticieros/raw/master/Analisis%20sentimientos%20tweets%20tiempo.png'>
+			<img width="50%" height="auto" src='https://upload.wikimedia.org/wikipedia/commons/e/e4/Twitter_2012_logo.svg'>
 			</div>
-			<br><br>
 			Twitter es actualmente una dinámica fuente de contenidos que, dada su popularidad e impacto, se ha convertido en uno de los principales medios de difusión de los principales medios de comunicación tradicionales (radio y televisión).
 			<br><br>
 			A continuación se realiza un análisis de sentimientos de los tweets de noticieros chilenos para así concluir si existe algún patrón entre los sentimientos evocados por los tweets de los noticieros. Para ello, se analizan las publicaciones que han hecho en Twitter los noticieros de @CNNChile, @ahoranoticiasAN, @24HorasTVN, @T13 y @CHVNoticias.
