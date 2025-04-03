846331bc30dc4266cc96aabb726d00c7a0414aa6
desareca <43764566+desareca@users.noreply.github.com>
2025-04-03 20:00:26 -0300

    fix image analisis-consumidores-mall

--- src/lib/data/projects.ts
+++ src/lib/data/projects.ts
@@ -343,7 +343,11 @@ const items: Array<Project> = [
 		slug: 'analisis-consumidores-mall',
 		color: 'steelblue',
 		description:
-			'Regresión para predecir el puntaje de los clientes en un mall.<br>Para el análisis se considerará:<br><ul><li>Un breve análisis exploratorio de los datos.<li>Dividir los datos en conjunto de entrenamiento y validación (80%-20%).<li>Con esto se realizarán diversos algoritmos de regresión mediante ternsorflow, donde el entrenamiento se realizará utilizando validación cruzada aleatoria (en cada algoritmo hay una descripción del procedimiento utilizado).<li>El conjunto de validación se utilizará para comparar el RMSE de cada método.</ul>',
+			`<div style="display: flex; justify-content: center; text-align: center;">
+			<img width="70%" height="auto" src='https://images.pexels.com/photos/54581/escalator-stairs-metal-segments-architecture-54581.jpeg'>
+			</div>
+			<br><br>
+			Regresión para predecir el puntaje de los clientes en un mall.<br>Para el análisis se considerará:<br><ul><li>Un breve análisis exploratorio de los datos.<li>Dividir los datos en conjunto de entrenamiento y validación (80%-20%).<li>Con esto se realizarán diversos algoritmos de regresión mediante ternsorflow, donde el entrenamiento se realizará utilizando validación cruzada aleatoria (en cada algoritmo hay una descripción del procedimiento utilizado).<li>El conjunto de validación se utilizará para comparar el RMSE de cada método.</ul>`,
 		shortDescription:
 			'Regresión para predecir el puntaje de los clientes en un mall.',
 		links: [
