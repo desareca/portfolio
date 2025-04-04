ebf6449ead7724bd48b2ab05c8b5f23394b730d7
desareca <43764566+desareca@users.noreply.github.com>
2025-04-03 21:21:06 -0300

    fix image erupcion volcanica

--- src/lib/data/projects.ts
+++ src/lib/data/projects.ts
@@ -66,7 +66,11 @@ const items: Array<Project> = [
 		slug: 'prediccion-erupcion-volcanica',
 		color: 'royalblue',
 		description:
-			`El Instituto Nacional de Geofísica y Vulcanología (INGV) de Italia se centra en la geofísica y la vulcanología. Su principal objetivo es contribuir a la comprensión del sistema terrestre y, al mismo tiempo, mitigar los riesgos asociados. Encargado de monitorear la sismicidad y la actividad volcánica activa las 24 horas del día en todo el país, el INGV busca encontrar los precursores detectables más tempranos que proporcionen información sobre la cronología de futuras erupciones volcánicas.
+			`<div style="display: flex; justify-content: center; text-align: center;">
+			<img width="70%" height="auto" src="https://images.pexels.com/photos/13337517/pexels-photo-13337517.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2">
+			</div>
+			<br><br>
+			El Instituto Nacional de Geofísica y Vulcanología (INGV) de Italia se centra en la geofísica y la vulcanología. Su principal objetivo es contribuir a la comprensión del sistema terrestre y, al mismo tiempo, mitigar los riesgos asociados. Encargado de monitorear la sismicidad y la actividad volcánica activa las 24 horas del día en todo el país, el INGV busca encontrar los precursores detectables más tempranos que proporcionen información sobre la cronología de futuras erupciones volcánicas.
 			<br><br>
 			El objetivo es predecir cuándo ocurrirá la próxima erupción de un volcán, para ello se analiza un amplio conjunto de datos geofísicos recopilados por sensores instalados en volcanes activos, calculando features de las series temporales para entrenar un modelo XGBoost que prediga cuando será la próxima erupción.`,
 		shortDescription:
